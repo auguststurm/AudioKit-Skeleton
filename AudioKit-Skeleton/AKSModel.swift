9d47c08b03347884832abc95f04bd99f3ee92449
August Sturm <io@auguststurm.com>
2018-07-01 14:56:03 -0700

    sample data and start of model

---
+++ AudioKit-Skeleton/AKSModel.swift
@@ -0,0 +1,16 @@
+//
+//  AKSModel.swift
+//  AudioKit-Skeleton
+//
+//  Created by August Sturm on 7/1/18.
+//  Copyright © 2018 Audio Kit Skeleton. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class AKSModel {
+	
+	
+	
+}
